c6a31ea2b5c4e51692be0ea85f562ba44dc09b37
Zap0xfce2 <>
2022-03-06 18:39:38 +0100

    Initialer Commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import Console
+
+if len(sys.argv) <= 1:
+    Console.error('Es muss ein Argument übergeben werden!')
+    Console.info('1.) Restbetrag der über bleiben soll')
+    Console.info('Beispielaufruf: ' + sys.argv[0] + ' 32\n')
+    sys.exit()
+else:
+    MoneyBack = float(sys.argv[1].replace(',', '.'))
+
+URL = 'https://www.online-gebuehrenrechner.de/paypal.php'
+RequestData = {'sales2': MoneyBack, 'submitbutton': 'Berechnen'}
+RequestPost = requests.post(URL, data=RequestData)
+soup = BeautifulSoup(RequestPost.text, 'html.parser')
+HTML_Tags = soup.find_all(class_="paypal1")
+Result = HTML_Tags[1].string[:-3].strip()
+print(Result + "€")
